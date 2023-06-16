1b790389562a5578ca53c7d114761e1578bef8c2
ppysl3 <116172763+ppysl3@users.noreply.github.com>
2023-06-16 11:08:21 +0100

    Modify trainer to be nnUNetTrainer based

--- training/nnUNetTrainer/variants/stephen/nnUNetTrainerClusterLoad.py
+++ training/nnUNetTrainer/variants/stephen/nnUNetTrainerClusterLoad.py
@@ -3,7 +3,25 @@ import numpy as np
 from nnunetv2.training.nnUNetTrainer.variant.data_augmentation import nnUNetTrainerNoDA
 from nnunetv2.training.dataloading import nnUNetClusterDataLoader2D
 import sys
-class nnUNetTrainerClusterLoad(nnUNetTrainerNoDA):
+from typing import Union, Tuple, List
+from batchgenerators.transforms.abstract_transforms import AbstractTransform
+class nnUNetTrainerClusterLoad(nnUNetTrainer):
+    @staticmethod
+     def get_training_transforms(patch_size: Union[np.ndarray, Tuple[int]],
+                                rotation_for_DA: dict,
+                                deep_supervision_scales: Union[List, Tuple],
+                                mirror_axes: Tuple[int, ...],
+                                do_dummy_2d_data_aug: bool,
+                                order_resampling_data: int = 1,
+                                order_resampling_seg: int = 0,
+                                border_val_seg: int = -1,
+                                use_mask_for_norm: List[bool] = None,
+                                is_cascaded: bool = False,
+                                foreground_labels: Union[Tuple[int, ...], List[int]] = None,
+                                regions: List[Union[List[int], Tuple[int, ...], int]] = None,
+                                ignore_label: int = None) -> AbstractTransform:
+        return nnUNetTrainer.get_validation_transforms(deep_supervision_scales, is_cascaded, foreground_labels,
+                                                       regions, ignore_label)
     def get_plain_dataloaders(self, initial_patch_size: Tuple[int, ...], dim: int):
         
         #From NoDA's version of plain_dataloaders, adding back in case its overwritten
@@ -29,3 +47,10 @@ class nnUNetTrainerClusterLoad(nnUNetTrainerNoDA):
             print("UNSUITABLE DIMENSIONS")
             sys.exit()
         return dl_tr, dl_val
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        # we need to disable mirroring here so that no mirroring will be applied in inferene!
+        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
+            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
