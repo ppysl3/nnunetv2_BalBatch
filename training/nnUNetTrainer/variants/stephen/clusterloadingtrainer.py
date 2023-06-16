ba34a38358e155f21699851d01fabdd95b53d6d6
ppysl3 <116172763+ppysl3@users.noreply.github.com>
2023-06-16 09:36:43 +0100

    Add cluster loader into noDA trainer

--- training/nnUNetTrainer/variants/stephen/clusterloadingtrainer.py
+++ training/nnUNetTrainer/variants/stephen/clusterloadingtrainer.py
@@ -1,1 +1,26 @@
-#hello
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+import numpy as np
+from nnunetv2.training.nnUNetTrainer.variant.data_augmentation import nnUNetTrainerNoDA
+from nnunetv2.training.dataloading import nnUNetClusterDataLoader2D
+import sys
+class nnUNetTrainerClusterLoad(nnUNetTrainerNoDA):
+    def get_plain_dataloaders(self, initial_patch_size: Tuple[int, ...], dim: int):
+        dataset_tr, dataset_val = self.get_tr_and_val_datasets()
+        #We only want to modify the training loader
+        if dim == 2:
+            dl_tr = nnUNetClusterDataLoader2D(dataset_tr, self.batch_size,
+                                       initial_patch_size,
+                                       self.configuration_manager.patch_size,
+                                       self.label_manager,
+                                       oversample_foreground_percent=self.oversample_foreground_percent,
+                                       sampling_probabilities=None, pad_sides=None)
+            dl_val = nnUNetDataLoader2D(dataset_val, self.batch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.label_manager,
+                                        oversample_foreground_percent=self.oversample_foreground_percent,
+                                        sampling_probabilities=None, pad_sides=None)
+        else:
+            print("UNSUITABLE DIMENSIONS")
+            sys.exit()
+        return dl_tr, dl_val
